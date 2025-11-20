f69d7dc2797fd9b60e6cd526340a8ead107f8e6f
Muhamad Arjuna <stoopidnigg24@gmail.com>
2025-11-21 05:51:07 +0700

    Update Web ARJUNA

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 ## Next.js App Router Course - Starter
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
 
 For more information, see the [course curriculum](https://nextjs.org/learn) on the Next.js Website.
-=======
-# web-teknologi
->>>>>>> 8d92dae01019662eac1662e2c9718a0a0941228c
--- app/page.tsx
+++ app/page.tsx
@@ -7,24 +7,25 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-800  p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
         <Image
           src="/logo/uia-logo.png"
-          alt="Banner UIA"
           width={500}
           height={360}
+          className='hidden md:block'
+          alt='Banner UIA'
         />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-       
-          <p className={`${ lusitana.className}text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Muhamad Arjuna Wicaksono</strong> NIM:3420220016{' '}
+
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+            <strong>Selamat Datang!</strong> <br></br>Ini adalah situs web pembelajaran{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Mata Kuliah Web Teknologi
+              Mata kuliah web teknologi
             </a>
-            ,Dosen Pengampu <b>Adhitya Ahmad Praditya, S.Kom.,M.T.I</b>
+            , Dosen Pengampu <b>Adhitya Ahmad Pradypta, S.Kom., M.T.I.</b>
           </p>
           <Link
             href="/login"
