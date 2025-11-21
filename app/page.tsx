9913946626111cc594682d8fcb96da15b8f39e1f
Muhamad Arjuna <stoopidnigg24@gmail.com>
2025-11-21 15:05:45 +0700

    Update sampul

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
 
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Selamat Datang!</strong> <br></br>Ini adalah situs web pembelajaran{' '}
+            <strong>Muhamad Arjuna Wicaksono</strong> <br></br>Ini adalah situs web pembelajaran{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Mata kuliah web teknologi
             </a>
