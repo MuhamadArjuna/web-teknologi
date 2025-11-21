650c4cc411177163530952f87afd597dcae294b4
Muhamad Arjuna <stoopidnigg24@gmail.com>
2025-11-21 14:58:33 +0700

    Update Image

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -15,7 +15,7 @@ export default function SideNav() {
         <div className="w-32 text-white md:w-40">
           {/*<AcmeLogo />*/}
           <Image
-            src="/logo/uia-logo.png"
+            src="/Logo/uia-logo.png"
             width={500}
             height={160}
             className='hidden md:block'
